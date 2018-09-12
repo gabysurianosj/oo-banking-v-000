0b3d2234e6adff773df74910a148926db16a407e
Gabriela Suriano <surianogabriela@gmail.com>
2018-09-12 11:26:31 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -7,5 +7,21 @@ class BankAccount
     @balance = 1000
     @status = "open"
   end 
+  
+  def deposit(money)
+    @balance += money 
+  end 
+  
+  def display_balance
+    "Your balance is $#{balance}."
+  end 
+  
+  def valid 
+    @status == "open" && @balane > 0 ? true : false 
+  end 
+  
+  def close_account 
+    @status = "closed"
+  end 
     
 end
